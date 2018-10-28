a6cb9b17843a8110a1c8aad72c375fe3d715d8f9
ashking <ashking25@gmail.com>
2018-10-28 11:31:58 -0700

    changed conv1d to conv2d

--- TCN_code.py
+++ TCN_code.py
@@ -2,7 +2,7 @@
 import numpy as np
 import keras
 from keras.models import Sequential
-from keras.layers import Dense, Conv1D
+from keras.layers import Dense, Conv2D
 from keras.layers import Flatten, Input
 from keras.layers import Add, Dropout, BatchNormalization
 from keras.models import *
@@ -19,25 +19,24 @@ def ResidualBlock(inputs, n_outputs, k, d, dropout_rate):
 
     return
     f: final layer in residual block"""
-    input_dim = int(inputs.shape[2])
-    c1 = Conv1D(n_outputs, kernel_size=k, dilation_rate=d,\
-                input_shape=(input_dim,1), activation='relu',\
+
+    c1 = Conv2D(n_outputs, kernel_size=k, dilation_rate=d, activation='relu',\
                padding='same', kernel_initializer=RandomNormal(mean=0, stddev=0.01))(inputs)
     b1 = BatchNormalization()(c1)
-    d1 = Dropout(dropout_rate, noise_shape=(1, 1, n_outputs))(b1)
-    c2 = Conv1D(n_outputs, kernel_size=k, dilation_rate=d,
+    d1 = Dropout(dropout_rate, noise_shape=(1, 1, 1, n_outputs))(b1)
+    c2 = Conv2D(n_outputs, kernel_size=k, dilation_rate=d,
                 activation='relu', padding='same',
                 kernel_initializer=RandomNormal(mean=0, stddev=0.01))(d1)
     b2 = BatchNormalization()(c2)
-    d2 = Dropout(dropout_rate, noise_shape=(1, 1, n_outputs))(b2)
+    d2 = Dropout(dropout_rate, noise_shape=(1, 1, 1, n_outputs))(b2)
     e = Dense(n_outputs, activation=None)(inputs)
     f = Add()([e, d2])
-    g = Dense(n_outputs, activation='relu', name='ResidBlock_'+str(d),
+    g = Dense(n_outputs, activation='relu', name='ResidBlock_'+str(d[0]),
         kernel_initializer=RandomNormal(mean=0, stddev=0.01))(f)
     return g
 
 
-def TCN(input_dim, time_steps, layers, features, dilation_rate=2., kernel_size=3, dropout=0.):
+def TCN(input_dim, time_steps, layers, features, dilation_rate=2., kernel_size=(3,1), dropout=0.):
     """ Temporal Convolution Neural Network
         By using dilaiton with enough layers, all values should be causally
         connnected"""
@@ -48,14 +47,14 @@ def TCN(input_dim, time_steps, layers, features, dilation_rate=2., kernel_size=3
     inputs = Input(shape=input_dim)
 
     for i in range(num_levels):
-        dilation_size = int(dilation_rate ** i)
+        dilation_size = (int(dilation_rate ** i),1)
 
         out_channels = num_channels[i]
         if i == 0:
             mod = ResidualBlock(inputs, out_channels, kernel_size, dilation_size, dropout)
         else:
             mod = ResidualBlock(mod, out_channels, kernel_size, dilation_size, dropout)
-    cEnd = Conv1D(1, kernel_size=1, dilation_rate=1, activation='sigmoid',\
+    cEnd = Conv2D(1, kernel_size=(1,1), dilation_rate=1, activation='sigmoid',\
                padding='same', kernel_initializer=RandomNormal(mean=0, stddev=0.01))(mod)
     bcEnd = BatchNormalization()(cEnd)
     mod1 = Flatten()(bcEnd)
@@ -82,7 +81,7 @@ def dataloader(batch_size=10, nstart=0, num_eq=1000, num_days=30, PATH=''):
                 data += [np.load(PATH+'/EQ'+str(EQ)+'_'+str(day)+'daysuntilEQ.npy')]
                 y += [day]
 
-            data = np.reshape(data, (len(data), np.shape(data)[1], 1))
+            data = np.reshape(data, (len(data), np.shape(data)[1], 1, 1))
             y = np.array(y)
 
             yield (data, y)
