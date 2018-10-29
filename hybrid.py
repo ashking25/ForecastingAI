c544d78ba5415365cfd34d68dd11747ff99ca55d
ashking <ashking25@gmail.com>
2018-10-29 12:52:19 -0700

    adding lstm relu

--- hybrid.py
+++ hybrid.py
@@ -129,7 +129,7 @@ def my_model(input_dim, time_steps, layers, features, n_hidden,
     dense1 = Dense(256, activation='relu')(resh)
 
     #lstm1=LSTM(features , return_sequences=True, activation='tanh')(dense1)
-    lstm2=LSTM(1, activation='linear')(dense1)
+    lstm2=LSTM(1, activation='relu')(dense1)
     #mod1 = Flatten()(dense1)
     #mod3 = Dense(1, activation='relu', kernel_initializer=RandomNormal(mean=0, stddev=0.01))(mod1) # the last output should be able to reach all of y values
 
@@ -192,6 +192,6 @@ callbacks = keras.callbacks.ModelCheckpoint(filepath, monitor='val_loss',
 model2.fit_generator(train_gen, steps_per_epoch=steps_per_epoch, epochs=20, verbose=2, \
         validation_data=test_data, callbacks=[callbacks])
 
-K.set_value(model2.optimizer.lr, 1e-5)
+K.set_value(model2.optimizer.lr, 1e-7)
 model2.fit_generator(train_gen, steps_per_epoch=steps_per_epoch, epochs=epochs,
         verbose=2, validation_data=test_data, callbacks=[callbacks])
