da695c1ae34575217f72019eb5e163f3d3f59b3d
ashking <ashking25@gmail.com>
2018-10-29 11:55:17 -0700

    adding another dense layer

--- hybrid.py
+++ hybrid.py
@@ -125,13 +125,13 @@ def my_model(input_dim, time_steps, layers, features, n_hidden,
     mod2 = TimeDistributed(Dense(1, activation='relu', \
         kernel_initializer=RandomNormal(mean=0, stddev=0.01)))(resh) # the last output should be able to reach all of y values
 
-
-    glob_pool = TimeDistributed(GlobalMaxPooling1D())(mod)
+    #glob_pool = TimeDistributed(GlobalMaxPooling1D())(mod)
+    dense1 = Dense(256, activation='relu')(resh)
 
     #lstm1=LSTM(features , return_sequences=True, activation='tanh')(mod2)
     #lstm2=LSTM(1, activation='linear')(glob_pool)
-    mod1 = Flatten()(glob_pool)
-    mod3 = Dense(1, activation='linear', kernel_initializer=RandomNormal(mean=0, stddev=0.01))(mod1) # the last output should be able to reach all of y values
+    mod1 = Flatten()(dense1)
+    mod3 = Dense(1, activation='relu', kernel_initializer=RandomNormal(mean=0, stddev=0.01))(mod1) # the last output should be able to reach all of y values
 
     resh2 = Flatten()(mod2)
     mod_end = Concatenate()([mod3,resh2])
