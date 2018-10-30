94325d15456c0beb1af7fcf8dce63c2b0afa9d68
ashking <ashking25@gmail.com>
2018-10-29 18:36:18 -0700

    remove dense

--- hybrid.py
+++ hybrid.py
@@ -132,12 +132,12 @@ def my_model(input_dim, time_steps, layers, features, n_hidden,
     #dense1 = Dense(256, activation='relu')(resh)
 
     #lstm1=LSTM(features , return_sequences=True, activation='tanh')(dense1)
-    #lstm2=LSTM(1, activation=custom_activation)(glob_pool)
-    mod1 = Flatten()(glob_pool)
-    mod3 = Dense(1, activation='relu', kernel_initializer=RandomNormal(mean=0, stddev=0.01))(mod1) # the last output should be able to reach all of y values
+    lstm2=LSTM(1, activation=custom_activation)(glob_pool)
+    #mod1 = Flatten()(glob_pool)
+    #mod3 = Dense(1, activation='relu', kernel_initializer=RandomNormal(mean=0, stddev=0.01))(mod1) # the last output should be able to reach all of y values
 
     resh2 = Flatten()(mod2)
-    mod_end = Concatenate()([mod3,resh2])
+    mod_end = Concatenate()([lstm2,resh2])
     model = Model(input=[inputs], output=mod3)
     return model
 
