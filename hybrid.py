1e95cc9a8411f503876f25d9d8ad9fda380cf0b0
ashking <ashking25@gmail.com>
2018-11-01 21:25:34 -0700

    changing dense relu

--- hybrid.py
+++ hybrid.py
@@ -131,10 +131,10 @@ def my_model(input_dim, time_steps, layers, features, n_hidden,
         kernel_initializer=RandomNormal(mean=0, stddev=0.01)))(resh) # the last output should be able to reach all of y values
 
     #glob_pool = TimeDistributed(GlobalMaxPooling1D())(mod)
-    dense1 = Dense(256, activation='relu')(resh)
+    dense1 = Dense(128, activation='relu')(resh)
 
     #lstm1=LSTM(features , return_sequences=True, activation='tanh')(dense1)
-    lstm2=LSTM(1, activation=custom_activation)(dense1)
+    lstm2=LSTM(1, activation='relu')(dense1)
     #mod1 = Flatten()(glob_pool)
     #mod3 = Dense(1, activation='relu', kernel_initializer=RandomNormal(mean=0, stddev=0.01))(mod1) # the last output should be able to reach all of y values
 
