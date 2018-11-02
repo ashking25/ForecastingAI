04b1ff6c4ad03070239ae10098d90e19287d031b
ashking <ashking25@gmail.com>
2018-11-01 21:18:29 -0700

    changing lstm to dense

--- hybrid.py
+++ hybrid.py
@@ -131,7 +131,7 @@ def my_model(input_dim, time_steps, layers, features, n_hidden,
         kernel_initializer=RandomNormal(mean=0, stddev=0.01)))(resh) # the last output should be able to reach all of y values
 
     #glob_pool = TimeDistributed(GlobalMaxPooling1D())(mod)
-    dense1 = Dense(256, activation='relu')(mod)
+    dense1 = Dense(256, activation='relu')(resh)
 
     #lstm1=LSTM(features , return_sequences=True, activation='tanh')(dense1)
     lstm2=LSTM(1, activation=custom_activation)(dense1)
