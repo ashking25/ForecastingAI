6641346fc91e61c0958330a8a6f150a8fd78608b
ashking <ashking25@gmail.com>
2018-11-03 14:38:33 -0700

    add if __name__

--- hybrid.py
+++ hybrid.py
@@ -145,69 +145,70 @@ def my_model(input_dim, time_steps, layers, features, n_hidden,
     return model
 
 ### Data params ###
-batch_size = 2
-epochs = 200
-steps_per_epoch = 50# int(900/BATCH_SIZE)#*30
-timesteps = 1
-data_length = int(3600*24/timesteps)
-lookback = 5
-#input_dim = (None, int(data_length), 1)
-input_dim = (int(lookback*timesteps), int(data_length), 1)
-dropout = 0
-features = 1 # number of features in lstm
-n_hidden = 64 # number of featurs in TCN
-kernel_size  = 7
-dilation_rate = 4
-layers = int(np.ceil(np.log((input_dim[1]-1.)/(2.*(kernel_size-1))+1)/np.log(dilation_rate)))
-#Optimizer
-lr = 0.003
-### Model ###
-if True:
+if __name__ == "__main__":
+    batch_size = 2
+    epochs = 200
+    steps_per_epoch = 50# int(900/BATCH_SIZE)#*30
+    timesteps = 1
+    data_length = int(3600*24/timesteps)
+    lookback = 5
+    #input_dim = (None, int(data_length), 1)
+    input_dim = (int(lookback*timesteps), int(data_length), 1)
+    dropout = 0
+    features = 1 # number of features in lstm
+    n_hidden = 64 # number of featurs in TCN
+    kernel_size  = 7
+    dilation_rate = 4
+    layers = int(np.ceil(np.log((input_dim[1]-1.)/(2.*(kernel_size-1))+1)/np.log(dilation_rate)))
+    #Optimizer
+    lr = 0.003
+    ### Model ###
+    if True:
 
-    model2 = my_model(input_dim, timesteps, layers, features, n_hidden,
-            dilation_rate=dilation_rate, kernel_size=kernel_size, dropout=dropout)
-
-
-    adam = keras.optimizers.Adam(lr=lr, beta_1=0.9, beta_2=0.999, epsilon=None,
-        decay=0.01, amsgrad=False)
-    model2.compile(loss=mean_total_squared_error,
-        metrics=[binary_lstm_accuracy, binary_tcn_accuracy, mean_tcn_squared_error,
-        'accuracy'], optimizer=adam)
-else:
-    model2= load_model('../data/mocks/logs/model_hybrid_look1_l7_k7_nh64_d4_f1_lr0.003_sqerr.hdf5',
-        custom_objects={'binary_lstm_accuracy':binary_lstm_accuracy,'binary_tcn_accuracy':binary_tcn_accuracy,\
-        'mean_total_squared_error':mean_total_squared_error,'mean_tcn_squared_error':mean_tcn_squared_error})
-
-print(model2.summary())
-print('layers', layers)
-
-### Data ###
-train_gen = dataloader_2(timesteps, data_length, lookback=lookback, batch_size=batch_size,
-    num_eq=900, PATH='../data/mocks')
-test_gen  = dataloader_2(timesteps, data_length, lookback=lookback, batch_size=25,
-    nstart=901, num_eq=1000, PATH='/home/ashking/quake_finder/data/mocks') #
-test_data = next(test_gen)
-train_data = next(train_gen)
+        model2 = my_model(input_dim, timesteps, layers, features, n_hidden,
+                dilation_rate=dilation_rate, kernel_size=kernel_size, dropout=dropout)
 
 
-#tensorboard = TensorBoard(log_dir="../data/mocks/logs/hybrid_l"+str(layers)+\
-#    "_k"+str(kernel_size)+"_nh"+str(n_hidden)+"_d"+str(dilation_rate)+"_f"+
-#    str(features)+"_lr"+str(lr)+"_sqerr", \
-#    histogram_freq=0, write_images=True)
+        adam = keras.optimizers.Adam(lr=lr, beta_1=0.9, beta_2=0.999, epsilon=None,
+            decay=0.01, amsgrad=False)
+        model2.compile(loss=mean_total_squared_error,
+            metrics=[binary_lstm_accuracy, binary_tcn_accuracy, mean_tcn_squared_error,
+            'accuracy'], optimizer=adam)
+    else:
+        model2= load_model('../data/mocks/logs/model_hybrid_look1_l7_k7_nh64_d4_f1_lr0.003_sqerr.hdf5',
+            custom_objects={'binary_lstm_accuracy':binary_lstm_accuracy,'binary_tcn_accuracy':binary_tcn_accuracy,\
+            'mean_total_squared_error':mean_total_squared_error,'mean_tcn_squared_error':mean_tcn_squared_error})
 
-filepath = "../data/mocks/logs/model_hybrid_denseoutput_dense_look"+str(lookback)+"_l"+str(layers)+\
-    "_k"+str(kernel_size)+"_nh"+str(n_hidden)+"_d"+str(dilation_rate)+"_f"+\
-        str(features)+"_lr"+str(lr)+"_sqerr.hdf5"
+    print(model2.summary())
+    print('layers', layers)
 
-callbacks = keras.callbacks.ModelCheckpoint(filepath, monitor='val_loss',
-    verbose=0, save_best_only=False, save_weights_only=False, mode='auto', period=10)
+    ### Data ###
+    train_gen = dataloader_2(timesteps, data_length, lookback=lookback, batch_size=batch_size,
+        num_eq=900, PATH='../data/mocks')
+    test_gen  = dataloader_2(timesteps, data_length, lookback=lookback, batch_size=25,
+        nstart=901, num_eq=1000, PATH='/home/ashking/quake_finder/data/mocks') #
+    test_data = next(test_gen)
+    train_data = next(train_gen)
+
+
+    #tensorboard = TensorBoard(log_dir="../data/mocks/logs/hybrid_l"+str(layers)+\
+    #    "_k"+str(kernel_size)+"_nh"+str(n_hidden)+"_d"+str(dilation_rate)+"_f"+
+    #    str(features)+"_lr"+str(lr)+"_sqerr", \
+    #    histogram_freq=0, write_images=True)
+
+    filepath = "../data/mocks/logs/model_hybrid_denseoutput_dense_look"+str(lookback)+"_l"+str(layers)+\
+        "_k"+str(kernel_size)+"_nh"+str(n_hidden)+"_d"+str(dilation_rate)+"_f"+\
+            str(features)+"_lr"+str(lr)+"_sqerr.hdf5"
+
+    callbacks = keras.callbacks.ModelCheckpoint(filepath, monitor='val_loss',
+        verbose=0, save_best_only=False, save_weights_only=False, mode='auto', period=10)
 
 
 
-#model2.fit_generator(train_gen, steps_per_epoch=steps_per_epoch, epochs=20, verbose=2, \
-#        validation_data=test_data, callbacks=[callbacks])
+    #model2.fit_generator(train_gen, steps_per_epoch=steps_per_epoch, epochs=20, verbose=2, \
+    #        validation_data=test_data, callbacks=[callbacks])
 
 
-K.set_value(model2.optimizer.lr, 2e-3)
-model2.fit_generator(train_gen, steps_per_epoch=steps_per_epoch, epochs=epochs,
-        verbose=2, validation_data=test_data, callbacks=[callbacks])
+    K.set_value(model2.optimizer.lr, 2e-3)
+    model2.fit_generator(train_gen, steps_per_epoch=steps_per_epoch, epochs=epochs,
+            verbose=2, validation_data=test_data, callbacks=[callbacks])
